4590be73277d2d5c93f0f5663d8988a7e92019cb
Lov <quangdaist01@gmail.com>
2021-03-29 09:19:22 +0700

    Make the auto_send_email code look much cleaner

--- Auto_send_email/auto_send_email.py
+++ Auto_send_email/auto_send_email.py
@@ -3,7 +3,6 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from crawl_data import TrungTamDayKem_Scrapy
 
-# TODO: gửi thông tin về lớp dạy tiếng Anh phù hợp mỗi 1 giờ qua email
 
 SENDER = 'sender_email@gmail.com'
 PASSWORD = 'senders_password'
@@ -16,43 +15,18 @@ msg['To'] = RECEIVER
 msg['Subject'] = "Thông tin các khóa dạy thêm phù hợp"
 
 # get courses information
-response = TrungTamDayKem_Scrapy()
-plain_text = response.find_courses(loc="Thủ Đức", gen='Sinh viên Nam')
-html_data = response.find_courses(loc="Thủ Đức", gen='Sinh viên Nam', return_dict=True)
+response = TrungTamDayKem_Scrapy(loc="Thủ Đức", gen='Sinh viên Nam')
+plain_text = response.render_raw()
+html_text = response.render_html()
 
 # turn these in to plain/html MIMEtext objects
 part1 = MIMEText(plain_text, 'plain')
+part2 = MIMEText(html_text, 'html')
 
 # making html part
-# load html
-
-# Change to where index.html is.
-with open("C:/Users/quang/PycharmProjects/pythonProject3/Auto_send_email/mail-portfolio-master/index.html", encoding='utf-8') as html:
-    template = Template(html.read())
-html_renderred = template.render(location=html_data['location'],
-                                 gender=html_data['gender'],
-                                 courses=html_data['courses'])
-part2 = MIMEText(html_renderred, 'html')
 msg.attach(part1)
 msg.attach(part2)
 
-
-with SMTP('smtp.gmail.com', 587) as sever:
-    sever.starttls()
-    sever.login(SENDER, PASSWORD)
-    sever.send_message(msg)
-
-
-# setup the parameters of the message
-msg = MIMEMultipart()  # create a message
-msg['From'] = SENDER
-msg['To'] = RECEIVER
-msg['Subject'] = "Thông tin các khóa dạy thêm phù hợp"
-# get courses information
-data = TrungTamDayKem_Scrapy()
-message = data.find_courses(loc="Thủ Đức", gen="Sinh viên Nam")
-# add to the message body
-msg.attach(MIMEText(message, 'plain'))
 
 with SMTP('smtp.gmail.com', 587) as sever:
     sever.starttls()
--- Auto_send_email/crawl_data.py
+++ Auto_send_email/crawl_data.py
@@ -1,34 +1,50 @@
 import requests
 from bs4 import BeautifulSoup
+from jinja2 import Template
 
 
 class TrungTamDayKem_Scrapy:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.response = requests.get('https://www.trungtamdaykem.com/lop-day-hien-co-dang-list.html')
         self.soup = BeautifulSoup(self.response.content, 'html.parser')
         self.table = self.soup.select("#main-content-3col > div > div.panel-body > table > tr")
         self.table.pop(0)
+        self.kwargs = kwargs
 
-    def find_courses(self, loc=None, gen=None, return_dict=False):
+    def _select_info(self, row):
+        info = row.select('td')
+        code, subject_time, loc, *_, tutor_gender, _ = info
+        subject, time = subject_time.strong, subject_time.span
+        package = list(map(lambda x: x.text, (code, subject, time, loc, tutor_gender)))
+        return package
+
+    def render_raw(self, return_dict=False):
+        loc, gen, *_ = self.kwargs.values()
         matched_courses = {'location': loc, 'gender': gen, 'courses': []}
         for row in self.table:
-            info = row.select('td')
-            code = info[0].text
-            subject = info[1].strong.text
-            time = info[1].span.text
-            location = info[2].text
-            tutor_gender = info[6].text
+            code, subject, time, location, tutor_gender = self._select_info(row)
             if loc not in location:
                 continue
             if gen not in tutor_gender:
                 continue
-            temp_dict = {'code': code, 'subject': subject, 'time': time, 'location': location}
-            matched_courses['courses'].append(temp_dict.copy())
+            matched_courses['courses'].append({'code': code, 'subject': subject, 'time': time, 'location': location})
         if return_dict:
             return matched_courses
         else:
             return ''.join([f'{code} || {subject} \n{time} \n{location} \n\n' for course in matched_courses['courses']])
 
+    def render_html(self, file_path=None):
+        html_data = self.render_raw(return_dict=True)
+        with open(
+                file_path or "C:/Users/quang/PycharmProjects/pythonProject3/Auto_send_email/mail-portfolio-master/index.html",
+                encoding='utf-8') as html:
+            template = Template(html.read())
+        return template.render(location=html_data['location'],
+                               gender=html_data['gender'],
+                               courses=html_data['courses'])
+
+
 if __name__ == '__main__':
-    result = TrungTamDayKem_Scrapy()
-    print(result.find_courses(loc="Thủ Đức", gen="Sinh viên Nam"))
+    result = TrungTamDayKem_Scrapy(loc="Tân Phú", gen="Sinh viên Nữ")
+    courses = result.render_html()
+    print(courses)
