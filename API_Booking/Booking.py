c0892ae789d989606ef578f4c02764e2f15afe95
Lov <quangdaist01@gmail.com>
2021-08-23 15:59:49 +0700

    Change structure of the main class

--- API_Booking/Booking.py
+++ API_Booking/Booking.py
@@ -1,30 +1,41 @@
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions
+from rich.console import Console
+from rich.table import Table
 import config
 
+
 class BookingAPI:
-    def __init__(self, link):
-        self.matched_hotels = []
-        self._set_up_driver(link)
-        self._set_selector()
+    def __init__(self, min_reviews_score=0, min_reviews_number=0, max_price=None, hotel_only=False):
+        self._setAttrs(min_reviews_score=min_reviews_score,
+                       min_reviews_number=min_reviews_number,
+                       max_price=max_price,
+                       hotel_only=hotel_only)
+        self.all_hotels = []
+        self.extracted_hotels = []
 
     def __enter__(self):
         print("Finding hotels...")
-        self.hotel_result = self._driver.find_elements_by_class_name("sr_item_content.sr_item_content_slider_wrapper")
+        self._setUpDriver()
+        self._setSelector()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         print("Closing the browser")
-        self.end_session()
+        self._driver.quit();
 
-    def _set_selector(self):
+    def _setAttrs(_self, **kwargs):
+        for k, v in kwargs.items():
+            setattr(_self, k, v)
+
+    def _setSelector(self):
         self.HOTEL_NAME_CLASS = "sr-hotel__name"
         self.HOTEL_LINK_CLASS = "js-sr-hotel-link.hotel_name_link.url"
-        self.RATING_CLASS = "bui-review-score__badge"
-        self.NUM_RATING_CLASS = "bui-review-score__text"
+        self.REVIEWS_SCORE_CLASS = "bui-review-score__badge"
+        self.REVIEWS_NUMBER_CLASS = "bui-review-score__text"
         self.PRICE_CLASS = "bui-price-display__value.prco-inline-block-maker-helper"
 
-    def _set_up_driver(self, link):
+    def _setUpDriver(self):
         USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"
         self._chrome_options = ChromeOptions()
         self._chrome_options.add_argument(f"user-agent={USER_AGENT}")
@@ -33,80 +44,96 @@ class BookingAPI:
         self._chrome_options.add_argument("--window-size=1920x1080")
         self._driver = webdriver.Chrome(options=self._chrome_options,
                                         executable_path=config.CHROMEDRIVER_PATH)
-        self._driver.get(link)
 
-    def _get_text(self, hotel, cls_name, attr=None):
-        if attr == "href":
+    def _getText(self, hotel, cls_name, get_link=False):
+        if get_link:
             return hotel.find_element_by_class_name(cls_name).get_attribute("href")
         else:
             return hotel.find_element_by_class_name(cls_name).get_attribute("innerText").replace("\n", "")
 
-    def _convert(self, value, type):
-        if type == "rating":
+    def _parseNumeric(self, value, type):
+        if type == "reviews score":
             return float(value.replace(",", "."))
-        elif type == "num_rating":
+        elif type == "reviews number":
             return int(''.join(filter(lambda i: i.isdigit(), value)))
         elif type == "price":
             return int(''.join(filter(lambda i: i.isdigit(), value))) if value else 0
         else:
             return 0
 
-
-    def _is_suitable(self, value, criteria):
+    def _isSuitable(self, value, criteria):
         if criteria == "name":
             return "hotel" in value.lower()
-        if criteria in ("rating"):
-            return True if self._convert(value, criteria) >= self.rates_score else False
-        if criteria in ("num_rating"):
-            return True if self._convert(value, criteria) >= self.min_num_rating else False
-        if criteria in ("price"):
-            return True if self._convert(value, criteria) <= self.price_limit else False
+        if criteria == "reviews score":
+            return True if self._parseNumeric(value, criteria) >= self.min_reviews_score else False
+        if criteria == "reviews number":
+            return True if self._parseNumeric(value, criteria) >= self.min_reviews_number else False
+        if criteria == "price":
+            return True if self._parseNumeric(value, criteria) <= self.max_price else False
 
-    def end_session(self):
-        self._driver.quit();
+    def _getAllHotels(self, link):
+        self._driver.get(link)
+        self._driver.implicitly_wait(5)
+        all_hotels = (
+            self._driver.find_elements_by_class_name("sr_item_content.sr_item_content_slider_wrapper"))
+        return all_hotels
 
-    def get_hotels_info(self, rates_score=0, min_num_rating=0, price_limit=None, hotel_only=False):
-        self.rates_score = rates_score
-        self.min_num_rating = min_num_rating
-        self.price_limit = price_limit
-        for hotel in self.hotel_result:
-            name = self._get_text(hotel, self.HOTEL_NAME_CLASS)
-            if hotel_only and not self._is_suitable(name, "name"):
+    def _filterAndExtractInfo(self, hotels):
+        extracted_hotels = []
+        for hotel in hotels:
+            name = self._getText(hotel, self.HOTEL_NAME_CLASS)
+            if self.hotel_only and not self._isSuitable(name, "name"):
                 continue
-            rating = self._get_text(hotel, self.RATING_CLASS)
-            if not self._is_suitable(rating, "rating"):
+            reviews_score = self._getText(hotel, self.REVIEWS_SCORE_CLASS)
+            if not self._isSuitable(reviews_score, "reviews score"):
                 continue
-            num_rating = self._get_text(hotel, self.NUM_RATING_CLASS)
-            if not self._is_suitable(num_rating, "num_rating"):
+            reviews_number = self._getText(hotel, self.REVIEWS_NUMBER_CLASS)
+            if not self._isSuitable(reviews_number, "reviews number"):
                 continue
-            price = self._get_text(hotel, self.PRICE_CLASS)
-            if not self._is_suitable(price, "price"):
+            price = self._getText(hotel, self.PRICE_CLASS)
+            if not self._isSuitable(price, "price"):
                 continue
-            link = self._get_text(hotel, self.HOTEL_LINK_CLASS, attr="href")
+            link = self._getText(hotel, self.HOTEL_LINK_CLASS, get_link=True)
+            extracted_hotels.append((name, link, reviews_score, reviews_number, price))
+        return extracted_hotels
 
-            print(name, rating, num_rating, price)
-            self.matched_hotels.append((name, link, rating, num_rating, price))
+    def getFilteredHotels(self, link):
+        all_hotels = self._getAllHotels(link)
+        result = self._filterAndExtractInfo(all_hotels)
+        self.extracted_hotels.extend(result)
         return self
 
-    def render_raw(self):
-        result = f"Điều kiện:\n\tĐiểm tối thiểu: {self.rates_score}\n\tSố lượng đánh giá tối thiểu: {self.min_num_rating}\n\tGiá tối ta: {self.price_limit}\n\n"
-        for hotel in self.matched_hotels:
-            name, _, rating, num_rating, price = hotel
-            result += f"{name} || {rating} \n{num_rating} \n{price} \n\n"
+    def renderRaw(self):
+        result = f"Điều kiện:\n\tĐiểm tối thiểu: {self.min_reviews_score}\n\tSố lượng đánh giá tối thiểu: {self.min_reviews_number}\n\tGiá tối ta: {self.max_price}\n\n"
+        for hotel in self.extracted_hotels:
+            name, _, reviews_score, reviews_number, price = hotel
+            result += f"{name} || {reviews_score} \n{reviews_number} \n{price} \n\n"
         return result
 
-    def render_html(self):
+    def prettifyTerminal(self):
+        console = Console()
+        table = Table(title="Danh sách các khách sạn phù hợp trên Booking.com")
+        table.add_column("Tên", style="cyan")
+        table.add_column("Điểm reviews", justify="center", style="green")
+        table.add_column("Số reviews", justify="center", style="magenta")
+        table.add_column("Giá", justify="center", style="yellow")
+        for hotel in self.extracted_hotels:
+            name, _, reviews_score, reviews_number, price = hotel
+            table.add_row(name, reviews_score, reviews_number, price)
+        console.print(table)
+
+    def renderHtml(self):
         html = f'''<html>
         <head>
-        <h4>Điểm Review >= {self.rates_score}, Số lượng Review >= {self.min_num_rating}, Giá <= {self.price_limit}</h4>
+        <h4>Điểm Review >= {self.min_reviews_score}, Số lượng Review >= {self.min_reviews_number}, Giá <= {self.max_price}</h4>
         </head>
         <body>'''
-        for hotel in self.matched_hotels:
-            name, link, rating, num_rating, price = hotel
+        for hotel in self.extracted_hotels:
+            name, link, reviews_score, reviews_number, price = hotel
             html += f'''
             <a href='{link}'>{name}</a>
-            <p>{rating}</p>
-            <p>{num_rating}</p>
+            <p>{reviews_score}</p>
+            <p>{reviews_number}</p>
             <p>{price}</p>
             <hr>
             '''
@@ -116,5 +143,6 @@ class BookingAPI:
 
 
 if __name__ == "__main__":
-    with BookingAPI(config.url) as bot:
-        bot.get_hotels_info(hotel_only=True, min_num_rating=30, rates_score=7, price_limit=420000)
+    with BookingAPI(hotel_only=True, min_reviews_number=30, min_reviews_score=7, max_price=420000) as bot:
+        bot.getFilteredHotels(config.url)
+        bot.prettifyTerminal()
--- API_Booking/Booking_Auto_Send.py
+++ API_Booking/Booking_Auto_Send.py
@@ -14,27 +14,28 @@ MIN_NUM_RATING = 30
 PRICE_LIMIT = 420000
 
 url = config.url
-with BookingAPI(url) as bot:
-    bot.get_hotels_info(RATES_SCORE, MIN_NUM_RATING, PRICE_LIMIT, hotel_only=True)
-    plain_text = bot.render_raw()
-    html = bot.render_html()
+with BookingAPI(RATES_SCORE, MIN_NUM_RATING, PRICE_LIMIT, hotel_only=True) as bot:
+    bot.getFilteredHotels(url)
+    bot.prettifyTerminal()
+    plain_text = bot.renderRaw()
+    html = bot.renderHtml()
 
-part1 = MIMEText(plain_text, "plain")
-part2 = MIMEText(html, "html")
+text_part = MIMEText(plain_text, "plain")
+html_part = MIMEText(html, "html")
 
-msg = MIMEMultipart("alternative")  # create a message
-msg.attach(part1)
-msg.attach(part2)
+message = MIMEMultipart("alternative")  # create a message
+message.attach(text_part)
+message.attach(html_part)
 # setup the parameters of the message
-msg['From'] = SENDER
-msg['To'] = RECEIVER
-msg['Subject'] = "Thông tin các KS phù hợp"
+message['From'] = SENDER
+message['To'] = RECEIVER
+message['Subject'] = "[Booking] Các KS phù hợp"
 
 
 with SMTP('smtp.gmail.com', 587) as sever:
     sever.starttls()
     sever.login(SENDER, PASSWORD)
-    sever.send_message(msg)
+    sever.send_message(message)
 
 # exit()
 
