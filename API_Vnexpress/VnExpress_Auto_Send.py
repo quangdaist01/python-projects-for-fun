02fd4cdb2cb9c992ca3f77199626fa047551f6d4
Lov <quangdaist01@gmail.com>
2021-08-28 14:24:26 +0700

    Add 2 crawling sources (tuoitre.vn, laodong.vn) Upgrade posts filter

--- API_Vnexpress/VnExpress_Auto_Send.py
+++ API_Vnexpress/VnExpress_Auto_Send.py
@@ -2,14 +2,21 @@ import sys
 sys.path.append(r"C:\\Users\quang\PycharmProjects\pythonProject3")
 import feedparser
 from emailsender import EmailSender
+import re
 
-feed = feedparser.parse('https://vnexpress.net/rss/tin-moi-nhat.rss')
-many_posts = feed.entries
+vnexpress_feed = feedparser.parse('https://vnexpress.net/rss/tin-moi-nhat.rss')
+laodong_feed = feedparser.parse('https://laodong.vn/rss/home.rss')
+tuoitre_feed = feedparser.parse('https://vnexpress.net/rss/tin-moi-nhat.rss')
+many_posts = vnexpress_feed.entries + laodong_feed.entries + tuoitre_feed.entries
 
 def is_vaccine_doses_post(post):
-    keywords = ["vaccine", "liều"]
-    title = post.title
-    return all(x in title for x in keywords)
+    doses_keywords = ["triệu", "ngàn"]
+    title = post.title.split()
+    if "vaccine" in title:
+        for i in range(len(title) - 1):
+            if title[i].isdigit() and title[i + 1] in doses_keywords:
+                return True
+    return False
 
 posts_info = ""
 for post in many_posts:
